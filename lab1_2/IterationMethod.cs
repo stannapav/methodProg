a32fb3606ea8aea45aa3725ea26b86fb65e30298
Anna Striharchuk <stannapav@gmail.com>
2024-03-06 10:42:54 +0200

    lab1.2 done

---
+++ lab1_2/IterationMethod.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+
+namespace lab1_2
+{
+    internal class IterationMethod
+    {
+        static double Epsilon = Math.Pow(10, -3);
+
+        public double equation1(double x)
+        {
+            return Math.Tan(0.44 * x + 0.3) - Math.Pow(x, 2);
+        }
+
+        public double equation2(double x)
+        {
+            return Math.Pow(x, 3) - 0.1 * Math.Pow(x, 2) + 0.4 * x + 1.2;
+        }
+
+        public double phi1(double x)
+        {
+            return Math.Sqrt(Math.Tan(0.44 * x + 0.3));
+        }
+
+        public double phi2(double x)
+        {
+            return Math.Cbrt(0.1 * Math.Pow(x, 2) - 0.4 * x - 1.2);
+        }
+
+        public double Iteration(double a, double b, Func<double, double> equation, Func<double, double> phi)
+        {
+            if (equation(a) * equation(b) >= 0)
+            {
+                MessageBox.Show("wrong interval");
+                return 0.0;
+            }
+
+            double prevX = a;
+            double x = (a + b) / 2;
+
+            if (Math.Abs(phi(x)) >= 1)
+            {
+                MessageBox.Show("this phi will not find root look for another phi");
+                return 0.0;
+            }
+
+            while (Math.Abs(prevX - x) >= Epsilon)
+            {
+                prevX = x;
+                x = phi(x);
+            }
+
+            if (equation(x - Epsilon) * equation(x + Epsilon) < 0)
+                return x;
+            else
+                return 0.0;
+        }
+    }
+}
---
+++ lab1_2/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace lab1_2
+{
+    public partial class MainWindow : Window
+    {
+        IterationMethod iterationMethod = new IterationMethod();
+        bool[,] changed = { { false, false }, { false, false } };
+        Regex numReg = new Regex(@"^[0-9]+$");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void firstBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double result = iterationMethod.Iteration(Convert.ToDouble(a1.Text), Convert.ToDouble(b1.Text), iterationMethod.equation1, iterationMethod.phi1);
+
+            Result1.Content = Convert.ToString(result);
+        }
+
+        private void secondBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double result = iterationMethod.Iteration(Convert.ToDouble(a2.Text), Convert.ToDouble(b2.Text), iterationMethod.equation2, iterationMethod.phi2);
+
+            Result2.Content = Convert.ToString(result);
+        }
+
+        private void a1_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            changed[0, 0] = (!string.IsNullOrEmpty(a1.Text) && numReg.IsMatch(a1.Text)) ? true : false;
+
+            firstBtn.IsEnabled = (changed[0, 0] && changed[0, 1]) ? true : false;
+        }
+
+        private void b1_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            changed[0, 1] = (!string.IsNullOrEmpty(b1.Text) && numReg.IsMatch(b1.Text)) ? true : false;
+
+            firstBtn.IsEnabled = (changed[0, 0] && changed[0, 1]) ? true : false;
+        }
+
+        private void a2_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            changed[1, 0] = (!string.IsNullOrEmpty(a2.Text) && numReg.IsMatch(a2.Text)) ? true : false;
+
+            secondBtn.IsEnabled = (changed[1, 0] && changed[1, 1]) ? true : false;
+        }
+
+        private void b2_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            changed[1, 1] = (!string.IsNullOrEmpty(b2.Text) && numReg.IsMatch(b2.Text)) ? true : false;
+
+            secondBtn.IsEnabled = (changed[1, 0] && changed[1, 1]) ? true : false;
+        }
+    }
+}
